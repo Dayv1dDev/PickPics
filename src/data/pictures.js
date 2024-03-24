13138255baffc29b7a03365145833b8d296b6239
Dayv1d <davidmorecam@gmail.com>
2024-03-24 15:11:38 +0100

    Add data folder and pictures.js file

---
+++ src/data/pictures.js
@@ -0,0 +1,47 @@
+export const pictures = [
+    {
+        id: 'galaxy-pic',
+        img: 'https://images.unsplash.com/photo-1464802686167-b939a6910659?q=80&w=2050&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        description: 'Fondo de pantalla digital de galaxias azules y moradas'
+    },
+    {
+        id: '',
+        img: '',
+        description: ''
+    },
+    {
+        id: '',
+        img: '',
+        description: ''
+    },
+    {
+        id: '',
+        img: '',
+        description: ''
+    },
+    {
+        id: '',
+        img: '',
+        description: ''
+    },
+    {
+        id: '',
+        img: '',
+        description: ''
+    },
+    {
+        id: '',
+        img: '',
+        description: ''
+    },
+    {
+        id: '',
+        img: '',
+        description: ''
+    },
+    {
+        id: '',
+        img: '',
+        description: ''
+    }
+]
